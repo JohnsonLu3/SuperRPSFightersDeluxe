48daebaa28c28b428fae96c405f8739bf49ba03b
JohnsonLu3 <JohnsonLu94@gmail.com>
2018-01-01 18:45:55 -0500

    created basic player controls and project structure

---
+++ Super RPS Fighter Deluxe/Assets/Resources/Sprites/Constants/Controls.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class Controls : MonoBehaviour {
+
+    public static string PLAYER1_LEFT     = "a";
+    public static string PLAYER1_RIGHT    = "s";
+    public static string PLAYER1_ROCK     = "d";
+    public static string PLAYER1_PAPER    = "f";
+    public static string PLAYER1_SCISSORS = "g";
+
+    public static string PLAYER2_LEFT = "j";
+    public static string PLAYER2_RIGHT = "k";
+    public static string PLAYER2_ROCK = "l";
+    public static string PLAYER2_PAPER = ";";
+    public static string PLAYER2_SCISSORS = "'";
+
+    public static string MENU = "esc";
+
+}
---
+++ Super RPS Fighter Deluxe/Assets/Resources/Sprites/Players/playerController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerController : MonoBehaviour {
+
+    bool rightFacing = false;
+    float fallSpeed = 5f;
+    float jumpForce = 5f;
+    public float moveSpeed = 2f;
+
+    Rigidbody2D rb;
+
+	// Use this for initialization
+	void Start () {
+        rb = GetComponent<Rigidbody2D>();	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKey(Controls.PLAYER1_LEFT)) {
+            rb.velocity = new Vector2(-moveSpeed, rb.velocity.y);
+        }
+        else if (Input.GetKey(Controls.PLAYER1_RIGHT))
+        {
+            rb.velocity = new Vector2(moveSpeed, rb.velocity.y);
+        } else if (Input.GetKey(Controls.PLAYER1_ROCK)) {
+
+        } else if (Input.GetKey(Controls.PLAYER1_PAPER)) {
+
+        }
+        else if (Input.GetKey(Controls.PLAYER1_SCISSORS))
+        {
+
+        }
+    }
+}
