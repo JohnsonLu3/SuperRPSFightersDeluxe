01c791c1fb17065d2416ac5baf25284c1fe14758
JohnsonLu3 <JohnsonLu94@gmail.com>
2018-01-02 00:20:13 -0500

    added basic jump

--- Super RPS Fighter Deluxe/Assets/Scripts/Player/playerController.cs
+++ Super RPS Fighter Deluxe/Assets/Scripts/Player/playerController.cs
@@ -16,24 +16,31 @@ public class playerController : MonoBehaviour {
     [SerializeField]
     float speedInc;
     [SerializeField]
+    LayerMask ground;
+    [SerializeField]
     float moveSpeed;
 
+    float groundDistance;
     Rigidbody2D rb;
+    BoxCollider2D bc;
 
 	// Use this for initialization
 	void Start () {
         rb = GetComponent<Rigidbody2D>();
+        bc = GetComponent<BoxCollider2D>();
         moveSpeed = defualtSpeed;
-	}
+        groundDistance = bc.bounds.extents.y;
+    }
 	
 	// Update is called once per frame
 	void Update () {
         float horizontal = Input.GetAxis("p1_Horizontal");
-        movementHandler(horizontal);
-
+        float vertical = Input.GetAxis("p1_Vertical");
+        movement(horizontal);
+        jump(vertical);
     }
 
-    void movementHandler(float horizontal) {
+    void movement(float horizontal) {
         if (horizontal == 0) {
             moveSpeed = defualtSpeed;
         }
@@ -41,5 +48,26 @@ public class playerController : MonoBehaviour {
             moveSpeed += speedInc;
         }
         rb.velocity = new Vector2(horizontal * moveSpeed, rb.velocity.y);
+    }
+
+    void jump(float vertical) {
+        if (vertical > 0 && IsGrounded()) {
+            rb.velocity = new Vector2(rb.velocity.x, vertical * jumpForce);
+        }
+    }
+
+    bool IsGrounded()
+    {
+        Vector2 position = transform.position;
+        Vector2 direction = Vector2.down;
+        float distance = 1.0f;
+
+        RaycastHit2D hit = Physics2D.Raycast(position, direction, distance, ground);
+        if (hit.collider != null)
+        {
+            return true;
+        }
+
+        return false;
     }
 }
