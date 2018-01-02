7bf2243e3295cb1d682cd327807ddb0257ccd8f4
JohnsonLu3 <JohnsonLu94@gmail.com>
2018-01-01 20:53:31 -0500

    tweak player movent and controllers to use unities built in control axis

---
+++ Super RPS Fighter Deluxe/Assets/Scripts/Player/playerController.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerController : MonoBehaviour {
+
+    bool rightFacing = false;
+    float defualtSpeed = 0f;
+
+    [SerializeField]
+    float fallSpeed;
+    [SerializeField]
+    float jumpForce;
+    [SerializeField]
+    float maxSpeed;
+    [SerializeField]
+    float speedInc;
+    [SerializeField]
+    float moveSpeed;
+
+    Rigidbody2D rb;
+
+	// Use this for initialization
+	void Start () {
+        rb = GetComponent<Rigidbody2D>();
+        moveSpeed = defualtSpeed;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float horizontal = Input.GetAxis("p1_Horizontal");
+        movementHandler(horizontal);
+
+    }
+
+    void movementHandler(float horizontal) {
+        if (horizontal == 0) {
+            moveSpeed = defualtSpeed;
+        }
+        if (moveSpeed < maxSpeed) {
+            moveSpeed += speedInc;
+        }
+        rb.velocity = new Vector2(horizontal * moveSpeed, rb.velocity.y);
+    }
+}
