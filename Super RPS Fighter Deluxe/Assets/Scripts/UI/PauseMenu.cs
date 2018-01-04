a2c79d8147e159c4b160bf75709e081de2c3fdc6
JohnsonLu3 <JohnsonLu94@gmail.com>
2018-01-03 19:33:06 -0500

    added pause buttons to quit and resume game

--- Super RPS Fighter Deluxe/Assets/Scripts/UI/PauseMenu.cs
+++ Super RPS Fighter Deluxe/Assets/Scripts/UI/PauseMenu.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PauseMenu : MonoBehaviour {
 
@@ -34,10 +35,10 @@ public class PauseMenu : MonoBehaviour {
     }
 
     public void quit() {
-
+        Application.Quit();
     }
 
     public void loadMenu() {
-
+        SceneManager.LoadScene("Menu");
     }
 }
