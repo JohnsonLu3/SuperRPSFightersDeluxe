6c3e49978026a6800b1302af1a4fdcf45f0a35bf
JohnsonLu3 <JohnsonLu94@gmail.com>
2018-01-03 19:08:55 -0500

    added pause menu, button currently not working

---
+++ Super RPS Fighter Deluxe/Assets/Scripts/UI/PauseMenu.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMenu : MonoBehaviour {
+
+    public static bool isPaused = false;
+    [SerializeField]
+    public GameObject pauseMenuUI;
+
+    void Update() {
+        if (Input.GetKeyDown(KeyCode.Escape)) {
+            if (isPaused)
+            {
+                resume();
+            }
+            else {
+                pause();
+            }
+        }
+    }
+
+
+    public void resume() {
+        pauseMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        isPaused = false;
+    }
+
+    public void pause() {
+        pauseMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+        isPaused = true;
+    }
+
+    public void quit() {
+
+    }
+
+    public void loadMenu() {
+
+    }
+}
